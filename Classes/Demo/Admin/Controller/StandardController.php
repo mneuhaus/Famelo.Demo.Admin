a8a25bfc0d9841604adeab048b0968d6614b225f
Marc Neuhaus <mneuhaus@famelo.com>
2012-10-04 15:55:19 +0200

    Updated to PSR-0

---
+++ Classes/Demo/Admin/Controller/StandardController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Demo\Admin\Controller;
+
+/*                                                                        *
+ * This script belongs to the FLOW3 package "Debug.Toolbar".              *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+use Debug\Toolbar\Annotations as Debug;
+
+/**
+ * Profile controller for the Debug.Toolbar package 
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class StandardController extends \TYPO3\FLOW3\Mvc\Controller\ActionController {
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+}
+
+?>
